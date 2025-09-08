edda761b0c1781a17731ba87e9a2d24eef7f70be
rayna806 <LUORA2@OOCL.COM>
2025-09-08 10:30:58 +0800

    commend:step1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,33 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s.I am 18 years %d.",name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
 }
--- src/test/java/oo/step1/PersonTest.java
+++ src/test/java/oo/step1/PersonTest.java
@@ -1,12 +1,33 @@
 package oo.step1;
 
+import oo.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PersonTest {
 
 
     // when introduce a person, then return a message with name and age
+    @Test
+    public void should_return_a_message_with_name_and_age_when_introduce_a_person() {
+        Person person = new Person(1, "Tom", 20);
+        assertEquals("My name is Tom.I am 18 years old.", person.introduce());
+    }
 
     // when equals two same id person then return true
+    @Test
+    public void should_return_true_when_equals_two_same_id_person() {
+        Person person = new Person(1, "Tom", 20);
+        Person person2 = new Person(1, "Tom", 20);
+        assertTrue(person.equals(person2));
+    }
 
     // when equals two different id person then return true
-
+    @Test
+    public void should_return_false_when_equals_two_different_id_person() {
+        Person person = new Person(1, "Tom", 20);
+        Person person2 = new Person(2, "Michael", 20);
+        assertFalse(person.equals(person2));
+    }
 }
